8ae9a10b71dd5bf979a5dc88fbecc3615bbb07ab
ewelinaczaja <114385389+ewelinaczaja@users.noreply.github.com>
2022-12-08 12:17:22 +0100

    Add files via upload

---
+++ src/app/dettaglio/dettaglio.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, } from '@angular/core';
+import { Post } from 'src/app/post';
+import { PostService } from 'src/app/post.service';
+import { ActivatedRoute } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-dettaglio',
+  templateUrl: './dettaglio.component.html',
+  styleUrls: ['./dettaglio.component.scss']
+})
+export class DettaglioComponent implements OnInit {
+
+  constructor(private service:PostService, private route:ActivatedRoute) { }
+  id!:number
+  data!:Post
+  ngOnInit(): void {
+    this.id=this.route.snapshot.params['id']
+    this.fetchPost()
+  }
+  fetchPost() {
+
+    this.service.fetchPost(this.id).then((data:Post) => {
+     this.data=data;
+    })
+
+
+   }
+
+}
